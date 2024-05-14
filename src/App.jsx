5cd0d40dd31f74fa48804a6d31c91e9fd9f8bf76
enzodp9 <102627346+enzodp9@users.noreply.github.com>
2024-05-14 12:32:55 -0300

    fix some conflicts

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,11 @@
 import { Component } from 'react';
-import AppRoutes from './AppRoutes';
-import { Routes, Route } from 'react-router-dom';
+import FirstPage from './components/FirstPage';
 
 export default class App extends Component {
 
     render() {
         return (
-            <Routes>
-                {AppRoutes.map((route, index) => {
-                    const { element, ...rest } = route;
-                    return <Route key={index} {...rest} element={element} />;
-                })}
-            </Routes>
+            <FirstPage />
         )
     }
 }
